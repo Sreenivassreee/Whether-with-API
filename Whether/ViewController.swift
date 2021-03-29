aa42cc37ed4325c36370a38674437bca4358a876
Sreenivassreee <ksreenivascse@gmail.com>
2021-03-29 23:54:39 +0530

    29/03/2020 working on backend

--- Whether/ViewController.swift
+++ Whether/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
 
+    @IBOutlet weak var SearchFiledText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        SearchFiledText.delegate=self
     }
 
-
+    @IBAction func searchFiels(_ sender: UITextField) {
+        print(sender.text!)
+    }
+    
+    @IBAction func SearchBtnPressed(_ sender: UIButton) {
+        print(SearchFiledText.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        SearchFiledText.endEditing(true)
+        
+        print(SearchFiledText.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        SearchFiledText.endEditing(true)
+        print(SearchFiledText.text!)
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        SearchFiledText.text=""
+    
+    }
+    
+    
 }
 
